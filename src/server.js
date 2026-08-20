2726cf3ab56b4b1a3fdc9a2e01bcef5d54e67db2
Aishwarya <eaishwarya273@gmail.com>
2026-08-20 18:10:01 +0800

    Prepare application for deployment

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import { app } from "./app.js"
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`)
 })
